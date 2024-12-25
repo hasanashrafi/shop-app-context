0e4b2ec5e4fff01e7f30a34119079ef2cac2fe5b
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-25 13:26:09 +0330

    `Removed 2 lines 25-40, changed lines 38 and 40 in Header.jsx`

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -25,13 +25,11 @@ function Header() {
     <>
       <div className="flex flex-wrap place-items-center  ">
         <section className="relative mx-auto">
-
           <nav className="flex justify-between bg-blue-600 text-white w-screen">
             <div className="px-5 xl:px-12 py-3 flex w-full items-center">
               <a className="text-4xl rounded font-bold font-heading" href="#">
               <FcShop className='' />
               </a>
-
               <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
                 <li><a className="hover:text-gray-200" href="#">Home</a></li>
                 <li><a className="hover:text-gray-200" href="#">Category</a></li>
@@ -40,7 +38,6 @@ function Header() {
               </ul>
 
               <div className="hidden xl:flex space-x-5 items-center">
-
                 <a className="flex items-center hover:text-gray-200" href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
