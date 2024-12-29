5dc8628a58c60c7b578ca7eee7f744b1cd3f96f2
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-29 11:18:38 +0330

    create app routes and added components

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../components/Home';
 import Layout from '../layout/Layout';
+import NotFoundPage from '../components/NotFoundPage';
+import ProductsPage from '../pages/ProductsPage';
+import DetailsPage from '../pages/DetailsPage';
+import CheckOutPage from '../pages/CheckOutPage';
 
 
 
 
 function Router() {
   return (
-   
-      <BrowserRouter> 
-         <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-         </Layout>
-      </BrowserRouter>
- 
+
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<DetailsPage />} />
+          <Route path="/checkout" element={<CheckOutPage />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+
   )
 }
 
