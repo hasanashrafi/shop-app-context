7faf24e8a0f82a4f19655aa50b1e2a62b14be56c
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-31 12:41:26 +0330

    added products page to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ProductsPage from '../pages/ProductsPage'
 
 function Home() {
   return (
-    <div className='min-h-screen'>Home</div>
+    <div className='min-h-screen mx-auto max-w-7xl flex justify-center'>
+    <ProductsPage/>
+    </div>
   )
 }
 
