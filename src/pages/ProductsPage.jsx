44793a56a7a7016f934b0f35074120a558ac589a
Hasan Ashrafi <yasashrafi@gmail.com>
2024-12-31 12:44:21 +0330

    create products page and map on products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useProducts } from '../context/ProductContext'
+import ProductCard from '../components/ProductCard'
 
 function ProductsPage() {
+  const  products  = useProducts()
+  console.log(products)
   return (
-    <div>ProductsPage</div>
+   <div className='w-full p-2 flex justify-center mx-auto '>
+     <div className='mx-auto flex flex-wrap  items-center  gap-3 justify-center'>
+      {
+       products && products.map((product) =>
+          <ProductCard key={product.id} product={product} />
+        )
+      }
+    </div>
+   </div>
   )
 }
 
