ccdf17a8d6e8bb09b25b109473e49b080dc9ebb2
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-04 21:04:47 +0330

    `Added searchProducts and filterProducts functions and modified shortestTitle function`

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,6 +1,24 @@
 const shortestTitle = (title) => {
-    const shortTitle = title.split(' ').slice(0, 18).join(' ');
-    return shortTitle
+  const words = title.split(' ');
+  return words.length > 3 ? words.slice(0, 3).join(' ') : title;
 }
 
-export { shortestTitle }
+const searchProducts = (products, search) => {
+  if (!search) return products;
+  const searchedProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(search.toLowerCase())
+  );
+  return searchedProducts;
+}
+
+const filterProducts = (products, category) => {
+    if (!category || category.toLowerCase() === 'all') return products;
+    const filteredProducts = products.filter(
+      (product) => product.category.name.toLowerCase() === category
+    );
+    return filteredProducts;
+  }
+
+
+
+export { shortestTitle, searchProducts, filterProducts }
