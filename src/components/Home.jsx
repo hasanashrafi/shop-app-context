8fdc092669601a8167e1fe7479bf6a08c29325b4
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-04 21:00:03 +0330

    added slider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ProductsPage from '../pages/ProductsPage'
+import Slider from './Slider'
 
 function Home() {
   return (
-    <div className='min-h-screen mx-auto max-w-7xl flex justify-center'>
+    <div className='min-h-screen mx-auto max-w-7xl flex flex-col justify-center'>
+    <Slider/>
     <ProductsPage/>
     </div>
   )
