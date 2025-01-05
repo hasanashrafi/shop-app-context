02d06b2569d500e9b58050659f02d19c7ffe3b5e
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-05 22:47:48 +0330

    edit styles and colors

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,18 +6,18 @@ import { BiDollar } from 'react-icons/bi';
 
 function ProductCard({ product }) {
     return (
-        <div className=" flex flex-col justify-between w-64 h-[350px] p-3  shadow-lg  rounded text-black">
-            <img src={product.images[1]} alt={product.title} className='rounded-md w-full h-full border object-cover ' />
+        <div className=" flex flex-col justify-between w-64 h-[400px] p-3  shadow-lg  rounded text-black">
+            <img src={product.images[1]} alt={product.title} className='rounded-md w-full h-1/2  border object-cover ' />
             <h2 className=" font-semibold">{product.title}</h2>
             <p className="text-sm text-gray-600 ">{shortestTitle(product.description)}</p>
            <p className='flex items-center '><BiDollar/> {product.price}</p>
           
           <div className=' p-3 text-2xl w-full flex items-center justify-between   '>
                 <Link to={`/products/${product.id}`} className=" text-gray-600 hover:text">
-                <TbShoppingBagExclamation className='hover:text-blue-700 transition-all ease-in-out' />
+                <TbShoppingBagExclamation className='hover:text-teal-700 transition-all ease-in-out' />
                 </Link>
             <button className='text-2xl text-gray-600'>
-                <TbShoppingBagPlus className='hover:text-blue-700 transition-all ease-in-out' />
+                <TbShoppingBagPlus className='hover:text-teal-700 transition-all ease-in-out' />
             </button>
            </div>
        
