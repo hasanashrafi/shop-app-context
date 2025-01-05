42a6800bd5d9a0cf77a2110c7d02e4ec8b18ac57
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-05 20:08:35 +0330

    remove white spaces

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,22 +7,18 @@ import { BiDollar } from 'react-icons/bi';
 function ProductCard({ product }) {
     return (
         <div className=" flex flex-col justify-between w-64 h-[350px] p-3  shadow-lg  rounded text-black">
-            <img src={product.images[1]} alt={product.title} className='rounded-md w-full h-36 border' />
+            <img src={product.images[1]} alt={product.title} className='rounded-md w-full h-full border object-cover ' />
             <h2 className=" font-semibold">{product.title}</h2>
             <p className="text-sm text-gray-600 ">{shortestTitle(product.description)}</p>
            <p className='flex items-center '><BiDollar/> {product.price}</p>
           
           <div className=' p-3 text-2xl w-full flex items-center justify-between   '>
-
                 <Link to={`/products/${product.id}`} className=" text-gray-600 hover:text">
                 <TbShoppingBagExclamation className='hover:text-blue-700 transition-all ease-in-out' />
                 </Link>
-          
             <button className='text-2xl text-gray-600'>
                 <TbShoppingBagPlus className='hover:text-blue-700 transition-all ease-in-out' />
-            
             </button>
-
            </div>
        
 
