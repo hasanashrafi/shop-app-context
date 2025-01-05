ae5f55a931131ff63543459b70363c0e9abbe867
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-05 22:48:38 +0330

    edit styles and z index

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,63 +1,52 @@
-import React, { useState, useEffect } from 'react'
-
+import React, { useState, useEffect } from 'react';
 import { useProducts } from '../context/ProductContext';
 import Loader from './Loader';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 function Slider() {
-    const products = useProducts()
-    const [isLoading, setIsLoading] = useState(true)
+    const products = useProducts();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         if (products) {
-            setIsLoading(false)
+            setIsLoading(false);
         }
-    }, [products])
+    }, [products]);
 
     if (isLoading) {
-        return <Loader />
+        return <Loader />;
     }
 
     return (
-
-        <div className='h-64 mx-auto w-full  p-2 my-5'>
-            <Swiper
-                effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={2}
-                coverflowEffect={{
-                    rotate: 50,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 1,
-                    slideShadows: true,
-                }}
-
-                modules={[EffectCoverflow, Pagination]}
-                className="mySwiper">
-                {
-                    products && products.map((product) =>
-
-                        <SwiperSlide 
-                        key={product.id} 
-                        className=' shadow-md  ' >
-                            <img src={product.images[1]} className='rounded-lg w-full md:h-96 h-56 ' />
-                        </SwiperSlide>
-                    )
-                }
-            </Swiper>
-        </div>
-
-    )
+        <div className='h-64 mx-auto w-full p-2 my-5 relative z-10'>
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={2}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          {products && products.map((product) => (
+            <SwiperSlide key={product.id} className='shadow-md'>
+              <img src={product.images[1]} className='rounded-lg w-full md:h-72 h-56' alt={product.title} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      
+    );
 }
 
-export default Slider
+export default Slider;
