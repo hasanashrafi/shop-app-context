387c6e92d673d61a713b0c88d23412575ad49cde
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-05 20:11:14 +0330

    handle all category and empty search

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -3,7 +3,7 @@ import { useProducts } from '../context/ProductContext';
 import ProductCard from '../components/ProductCard';
 import Loader from '../components/Loader';
 import { BiSearch } from 'react-icons/bi';
-import { filterProducts, searchProducts } from '../utils/helper';
+import { createQueryObject, filterProducts, searchProducts } from '../utils/helper';
 import { useSearchParams } from 'react-router-dom';
 
 function ProductsPage() {
@@ -25,57 +25,58 @@ function ProductsPage() {
     setDisplayed(finalProducts);
   }, [query, products]);
 
+
   const searchHandler = () => {
-    setQuery((query) => ({ ...query, search }));
+    setQuery((query) => createQueryObject(query, { search }));
   };
 
   const categoryHandler = (e) => {
     const { tagName } = e.target;
     if (tagName !== "LI") return;
     const category = e.target.textContent.toLowerCase();
-    setQuery((query) => ({ ...query, category }));
+    setQuery((query) => createQueryObject(query, { category }));
   };
 
   return (
     <div className='w-full p-2 flex flex-col justify-center mx-auto'>
-       
 
-        <div className='  p-1 flex justify-center mt-4 text-black  '>  
-          <ul className='w-full justify-center mx-auto flex flex-wrap p-2 items-center  md:flex md:justify-center gap-2' onClick={categoryHandler}>
-            <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
-              All
-            </li>
-            <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
-              Clothe
-            </li>
-            <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
+
+      <div className='  p-1 flex justify-center mt-4 text-black  '>
+        <ul className='w-full justify-center mx-auto flex flex-wrap p-2 items-center  md:flex md:justify-center gap-2' onClick={categoryHandler}>
+          <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
+            All
+          </li>
+          <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
+            Clothe
+          </li>
+          <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
             Electronicss
-            </li>
-            <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
-              Miscellaneous
-            </li>
-            <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
-              Shoes
-            </li>
-            <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
+          </li>
+          <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
+            Miscellaneous
+          </li>
+          <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
+            Shoes
+          </li>
+          <li className='w-32 md:w-28 md:text-sm shadow-md p-2 text-center bg-white hover:text-white   rounded-md cursor-pointer hover:bg-blue-700'>
             Furniture
-            </li>
-          </ul>
-        </div>
-     
+          </li>
+        </ul>
+      </div>
+
 
       <div className='m-4 justify-center flex items-center text-black '>
-          <input
-            onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
-            value={search}
-            type='text'
-            placeholder='search...'
-            className='w-1/2 outline-none border  rounded-md p-1'
-          />
-          <button onClick={searchHandler} className='text-3xl text-gray-500 mx-2'>
-            <BiSearch />
-          </button>
-        </div>
+        <input
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+          value={search}
+          type='text'
+          placeholder='search...'
+          className='w-1/2 outline-none border  rounded-md p-1'
+        />
+        <button onClick={searchHandler} className='text-3xl text-gray-500 mx-2'>
+          <BiSearch />
+        </button>
+      </div>
 
       <div className='mx-auto flex flex-wrap items-center gap-3 justify-center'>
         {!displayed && <Loader />}
