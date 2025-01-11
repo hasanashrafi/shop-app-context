8117f7be6b15f3b96849b13fe79aa850aa8fcbfb
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-11 19:49:23 +0330

    `Refactor CartContext to use useReducer and export useCart hook`

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { createContext, useContext, useReducer } from 'react'
+import { sumProducts } from '../utils/helper'
 
-function CartContext() {
+const initialState = {
+  selectedProducts: [],
+  productsCounter: 0,
+  total: 0,
+  checkout: false
+}
+
+const reducer = (state, action) => {
+  console.log(action)
+
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      if (!state.selectedProducts.find((item) => item.id === action.payload.id)) {
+        state.selectedProducts.push({ ...action.payload, quantity: 1 })
+      }
+      return {
+        ...state,
+        selectedProducts: [...state.selectedProducts],
+        checkout: false,
+        ...sumProducts(state.selectedProducts)
+      }
+
+    case 'REMOVE_PRODUCT':
+
+    case 'CHECKOUT':
+      checkout: true
+
+    default:
+      return state
+  }
+}
+
+
+const CartContext = createContext()
+
+function CartProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+
   return (
-    <div>CartContext</div>
+    <CartContext.Provider value={{ state, dispatch }}>
+      {children}
+    </CartContext.Provider>
   )
 }
 
-export default CartContext
+const useCart = () => {
+  const { state, dispatch } = useContext(CartContext)
+  return [state, dispatch]
+}
+
+
+export default CartProvider
+export { useCart }
