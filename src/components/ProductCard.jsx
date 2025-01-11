fb941b17deccf4bdff40cd4ac95e4f6c9b528c74
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-11 22:53:47 +0330

    `Refactor ProductCard component to include quantity management and improved UI`

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import { shortestTitle } from '../utils/helper';
+import { productQuantity, shortestTitle } from '../utils/helper';
 import { Link } from 'react-router-dom';
 import { TbShoppingBagExclamation, TbShoppingBagPlus, TbTrash } from 'react-icons/tb';
-import { BiDollar } from 'react-icons/bi';
+import { BiDollar, BiMinus, BiPlus } from 'react-icons/bi';
 import { useCart } from '../context/CartContext';
+import { FaTrash } from 'react-icons/fa';
 
 function ProductCard({ product }) {
     const [state, dispatch] = useCart()
+    const quantity = productQuantity(state, product.id)
+    console.log(quantity)
 
-    const handleAddToCart = () => {
-        dispatch({
-            type: 'ADD_TO_CART',
-            payload: product
-        })
+    const clickHandler = (type) => {
+        dispatch({ type, payload: product })
     }
 
-    const handleRemoveFromCart = () => {
-        dispatch({
-            type: 'REMOVE_PRODUCT',
-            payload: product
-        })
-    }
     console.log(state)
 
     return (
@@ -28,20 +22,44 @@ function ProductCard({ product }) {
             <img src={product.image} alt={product.title} className='rounded-md w-full h-52 mb-4' />
             <h2 className="font-semibold">{product.title}</h2>
             <p className="text-sm text-gray-600">{shortestTitle(product.description)}</p>
-            <p className='flex items-center'><BiDollar /> {product.price}</p>
+            <p className='flex items-center'><BiDollar />
+                {product.price}</p>
 
-            <div className='p-3 text-2xl w-full flex items-center justify-between'>
+            <div className='p-3  w-full flex items-center justify-between'>
                 <Link to={`/products/${product.id}`} className="text-gray-600 hover:text">
-                    <TbShoppingBagExclamation className='hover:text-teal-700 transition-all ease-in-out' />
+                    <TbShoppingBagExclamation className='text-2xl  hover:text-teal-700 transition-all ease-in-out' />
                 </Link>
-                <div className='flex items-center justify-between'>
-                    <button onClick={handleAddToCart}
-                        className='text-2xl text-gray-600'>
-                        <TbShoppingBagPlus className='hover:text-teal-700 transition-all ease-in-out' />
-                    </button>
-                    <button onClick={handleRemoveFromCart}>
-                        <TbTrash />
-                    </button>
+
+                <div className='flex  items-center justify-between'>
+                    {
+                        quantity === 0 ? (
+                            <button onClick={() => clickHandler("ADD_PRODUCT")}
+                                className='text-2xl text-gray-600'>
+                                <TbShoppingBagPlus className='text-2xl hover:text-teal-700 transition-all ease-in-out' />
+                            </button>
+                        ) : (
+                            <button onClick={() => clickHandler("INCREASE")}>
+                                <BiPlus className='text-xl rounded-full bg-green-600 text-white' />
+                            </button>
+                        )
+                    }
+
+
+                    {
+                        quantity === 1 && <button onClick={() => clickHandler("REMOVE_PRODUCT")}>
+                            <FaTrash className='text-xl text-gray-600 ml-1 hover:text-red-600 transition-all ease-in-out' />
+                        </button>
+                    }
+
+                    {quantity > 1 && <p className='mx-2 font-semibold'>{quantity}</p>}
+
+                    {
+                        quantity > 1 &&
+                        <button onClick={() => clickHandler("DECREASE")}>
+                            <BiMinus className='text-xl rounded-full bg-red-500 text-white' />
+                        </button>
+
+                    }
                 </div>
             </div>
         </div>
