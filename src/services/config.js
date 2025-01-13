5966b0fb625cce7e9705aab358d84d01761d277b
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-13 13:17:24 +0330

    refactor api config

--- src/services/config.js
+++ src/services/config.js
@@ -1,8 +1,22 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://fakestoreapi.com" })
+const api = axios.create({
+    baseURL: "https://fakestoreapi.com",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
 api.interceptors.response.use(
     (response) => response.data,
-    error => Promise.reject(error))
+    (error) => {
+        if (axios.isAxiosError(error)) {
+            console.error("Axios error:", error.message);
+        } else {
+            console.error("Unknown error:", error);
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default api;
