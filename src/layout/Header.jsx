a7fe75f96ce49f09b4309cdbdce2dd05ec759d30
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-13 13:19:00 +0330

    edit product counter

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { CiShop } from 'react-icons/ci';
 import BasketCart from '../modules/BasketCart';
+import { useCart } from '../context/CartContext';
 
 function Header() {
+  const [state] = useCart()
+
   const [isOpen, setIsOpen] = useState(false);
   const [openBasket, setOpenBasket] = useState(false);
   const menuRef = useRef(null);
@@ -58,21 +61,31 @@ function Header() {
                   <a className="hover:text-gray-200" href="/">Contact Us</a>
                 </li>
               </ul>
-
-
             </div>
 
-            <button className=" flex mr-6 items-center" onClick={() => setOpenBasket(!openBasket)}>
+            <button className="flex mr-6 items-center" onClick={() => setOpenBasket(!openBasket)}>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
               <span className="flex absolute -mt-5 ml-4">
-                <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
-                </span>
-              </span>
-            </button>
+                {
+                  state.productsCounter >= 1 &&
+                  <>
 
+                    <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-red-500 opacity-75">
+                    </span>
+                    <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500">
+                      <span className='text-[8px] mx-auto left-0 right-0 absolute bottom-0 top-0 my-auto'>
+                        {state.productsCounter}
+                      </span>
+                    </span>
+                  </>
+                }
+
+              </span>
+
+            </button>
+          
             {
               openBasket && (
                 <BasketCart basketRef={basketRef} />
@@ -85,9 +98,7 @@ function Header() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
             </a>
-
           </nav>
-
         </section>
       </div>
 
@@ -102,9 +113,7 @@ function Header() {
             </ul>
           </div>
         </div>
-      )
-      }
-
+      )}
     </div>
   )
 }
