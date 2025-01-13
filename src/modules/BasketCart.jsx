044eb52127e467af506372e005427c2571767106
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-13 20:58:47 +0330

    `Refactor Cart and Checkout functionality to use state.total instead of total variable`

--- src/modules/BasketCart.jsx
+++ src/modules/BasketCart.jsx
@@ -16,20 +16,20 @@ function BasketCart({ basketRef }) {
     }
 
     return (
-        <div className='absolute z-50   top-12 w-[84%] md:w-1/3 right-16 md:right-8 rounded-md h-64 bg-teal-800 p-2' ref={basketRef}>
+        <div className='absolute z-50 h-fit  top-12 w-[84%] md:w-1/3 right-16 md:right-8 rounded-md divide-y-2   bg-teal-800 p-2' ref={basketRef}>
             {selectedProducts.map((product, index) => (
-                <div key={index} className='flex items-center text-white justify-between p-2 border-b border-gray-200'>
+                <div key={index} className='flex items-center text-white justify-between  p-2  border-gray-200'>
                     <img src={product.image} className='rounded-full size-6'/>
                     <p>{shortestTitle(product.title)}</p>
                     <p>{product.price}</p>
                     <p className='rounded-full bg-orange-700 px-1'>x{product.quantity}</p>
                 </div>
             ))}
-            <div className='flex justify-between p-2 border-t border-gray-200'>
+            <div className='flex justify-between p-2  border-gray-200'>
                 <p>Total:</p>
-                <p>${total}</p>
+                <p>${state.total}</p>
             </div>
-            <Link className='absolute bottom-1 right-0 left-0 m-2 text-center p-1.5 hover:bg-teal-500 ease-in-out transition-all hover:text-white bg-white rounded-lg text-teal-700 '
+            <Link className=' mx-auto block m-2 text-center p-1.5 hover:bg-teal-500 ease-in-out transition-all hover:text-white bg-white rounded-lg text-teal-700 '
              to="/checkout">
             CheckOut
             </Link>
